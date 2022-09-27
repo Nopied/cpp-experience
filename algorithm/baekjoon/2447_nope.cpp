2b7f924c49c85ab1afe9d3a5307d12d86f9378b4
Nopied◎ <iuy0223@gmail.com>
2022-09-27 07:03:31 +0000

    later:tm:

---
+++ algorithm/baekjoon/2447_nope.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    std::cin.tie(NULL);
+    std::ios::sync_with_stdio(false);
+
+    int three; cin >> three;
+
+    string str;
+
+    
+
+
+    // with "-march=native -m64 -O3"
+    // much faster. https://stackoverflow.com/a/5296669
+    // int number = (int)sqrt((float)three);
+
+    return 0;
+}
